2eb8ff11597ae99e799f790a61fcb226807856c5
Sudhanshu Prasad <sudhanshuprasad.sp@gmail.com>
2025-04-05 14:46:48 +0530

    collector and vender

--- src/app/distributer/page.tsx
+++ src/app/distributer/page.tsx
@@ -2,7 +2,7 @@
 
 import { Button, Card, CardContent, InputAdornment, TextField, Typography } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Collector() {
 
@@ -14,6 +14,22 @@ export default function Collector() {
     tomato_inquiry: 'loading...',
     tomato_stock: 'loading...',
   });
+
+  const [allStock, setAllStock] = useState({ potato: 0, onion: 0, tomato: 0, potato_inquiry: 0, onion_inquiry: 0, tomato_inquiry: 0 });
+
+  const getAllStocks = async () => {
+    try {
+      const response: any = await axios.get(`/api/setCollectorData`);
+      console.log(response?.data);
+      setAllStock(response?.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    getAllStocks();
+  }, []);
 
   const handleSubmit = () => {
     // Handle form submission logic here
@@ -38,13 +54,14 @@ export default function Collector() {
 
           <ul className="list-disc list-inside mb-2 ml-5 flex flex-col gap-2">
             <li className='flex flex-row gap-2 justify-around text-center'>
+              <Typography sx={{ marginBottom: '10px', justifyContent: 'left' }} ></Typography>
               <Typography sx={{ marginBottom: '10px', justifyContent: 'left' }} >Stock:</Typography>
               <Typography sx={{ marginBottom: '10px', justifyContent: 'left' }}>Inquiry:</Typography>
-              <Typography sx={{ marginBottom: '10px', justifyContent: 'left' }}>Stock:</Typography>
+              <Typography sx={{ marginBottom: '10px', justifyContent: 'left' }}>Sell:</Typography>
             </li>
             <li className='flex flex-row gap-2 justify-around'>
               <Typography sx={{ width: '8rem' }}>Potato:</Typography>
-              <TextField variant="outlined" size='small' placeholder="0Kg" defaultValue={"0"}
+              <TextField variant="outlined" size='small' placeholder="0Kg" defaultValue={allStock.potato_inquiry}
                 onChange={(e) => {
                   console.log(e.target.value)
                   setFarmerData({ ...farmerData, potato_inquiry: e.target.value })
@@ -55,7 +72,18 @@ export default function Collector() {
                   },
                 }}
               />
-              <TextField variant="outlined" size='small' placeholder="0Kg" defaultValue={"0"}
+              <TextField variant="outlined" size='small' placeholder="0Kg" defaultValue={allStock.potato}
+                onChange={(e) => {
+                  console.log(e.target.value)
+                  setFarmerData({ ...farmerData, potato_stock: e.target.value })
+                }}
+                slotProps={{
+                  input: {
+                    endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+                  },
+                }}
+              />
+              <TextField variant="outlined" size='small' placeholder="0Kg" defaultValue={allStock.potato}
                 onChange={(e) => {
                   console.log(e.target.value)
                   setFarmerData({ ...farmerData, potato_stock: e.target.value })
@@ -70,7 +98,7 @@ export default function Collector() {
 
             <li className='flex flex-row gap-2 justify-around'>
               <Typography sx={{ width: '8rem' }}>Onion:</Typography>
-              <TextField variant="outlined" size='small' placeholder="0" defaultValue={"0"}
+              <TextField variant="outlined" size='small' placeholder="0" defaultValue={allStock.onion_inquiry}
                 onChange={(e) => {
                   console.log(e.target.value)
                   setFarmerData({ ...farmerData, onion_inquiry: e.target.value })
@@ -81,7 +109,18 @@ export default function Collector() {
                   },
                 }}
               />
-              <TextField variant="outlined" size='small' placeholder="0" defaultValue={"0"}
+              <TextField variant="outlined" size='small' placeholder="0" defaultValue={allStock.onion}
+                onChange={(e) => {
+                  console.log(e.target.value)
+                  setFarmerData({ ...farmerData, onion_stock: e.target.value })
+                }}
+                slotProps={{
+                  input: {
+                    endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+                  },
+                }}
+              />
+              <TextField variant="outlined" size='small' placeholder="0" defaultValue={allStock.onion}
                 onChange={(e) => {
                   console.log(e.target.value)
                   setFarmerData({ ...farmerData, onion_stock: e.target.value })
@@ -96,7 +135,7 @@ export default function Collector() {
 
             <li className='flex flex-row gap-2 justify-around'>
               <Typography sx={{ width: '8rem' }}>Tomato:</Typography>
-              <TextField variant="outlined" size='small' placeholder="0" defaultValue={"0"}
+              <TextField variant="outlined" size='small' placeholder="0" defaultValue={allStock.tomato_inquiry}
                 onChange={(e) => {
                   console.log(e.target.value)
                   setFarmerData({ ...farmerData, tomato_inquiry: e.target.value })
@@ -107,7 +146,18 @@ export default function Collector() {
                   },
                 }}
               />
-              <TextField variant="outlined" size='small' placeholder="0" defaultValue={"0"}
+              <TextField variant="outlined" size='small' placeholder="0" defaultValue={allStock.tomato}
+                onChange={(e) => {
+                  console.log(e.target.value)
+                  setFarmerData({ ...farmerData, tomato_stock: e.target.value })
+                }}
+                slotProps={{
+                  input: {
+                    endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+                  },
+                }}
+              />
+              <TextField variant="outlined" size='small' placeholder="0" defaultValue={allStock.tomato}
                 onChange={(e) => {
                   console.log(e.target.value)
                   setFarmerData({ ...farmerData, tomato_stock: e.target.value })
